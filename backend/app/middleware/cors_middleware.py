cc6de9c543431a456334bc42d35138d23e35227d
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2024-11-11 23:34:00 +0530

    set the cors origin

--- backend/app/middleware/cors_middleware.py
+++ backend/app/middleware/cors_middleware.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["http://localhost:5173","https://potato-disease-classifier-gold.vercel.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
